8f56ca80af77caf5537c2a71ada1c62f5e24562d
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2023-11-29 17:19:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const key= "e0283766cee7e7e70be7e75409ae8042"
+
+function buscar(){
+    let cidade = document.querySelector('.input-cidade').value
+    dados(cidade)
+}
+
+async function dados(cidade) {
+    let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json());
+    console.log(dados)
+}
+
