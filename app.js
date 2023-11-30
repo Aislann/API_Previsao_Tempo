be4e5c017babcaeb25a3d10147168c83c70a4087
Aislan Oliveira de Carvalho <89170451+Aislann@users.noreply.github.com>
2023-11-30 15:25:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ function buscar(){
 
 async function dados(cidade) {
     let dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json());
-    console.log(dados)
+    inserirDados(dados)
 }
 
+function inserirDados(dados) {
+    console.log(dados)
+    document.querySelector(".titulo-previsao").innerHTML ="Tempo em " + dados.name
+    document.querySelector(".graus").innerHTML ="Temperatura: " + Math.floor(dados.main.temp) +"°C"
+    document.querySelector(".texto-previsao").innerHTML ="Clima está: " + dados.weather[0].description
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png `  /// Mudar imagem 
+    document.querySelector(".Umidade").innerHTML ="Umidade: " + Math.floor(dados.main.humidity)+"%"
+
+}
